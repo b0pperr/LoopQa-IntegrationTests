5acba748235f45817bf13621f97cf07d6739b2fc
b0pperr <TannerLogan23@gmail.com>
2025-02-01 11:03:07 -0600

    removing some comment sections

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -36,20 +36,3 @@ class LoginPageAssertions extends BasePageAssertions {
 		await expect(this.loginPage._projectBoardLoginHeader, "Verifying user navigates to log in page").toBeVisible();
 	}
 }
-
-/*
-
-test('test', async ({ page }) => {
-  await page.goto('https://animated-gingersnap-8cf7f2.netlify.app/');
-  await expect(page.getByRole('heading', { name: 'Project Board Login' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
-  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Password' }).fill('password123');
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await expect(page.getByRole('heading', { name: 'Projects' })).toBeVisible();
-  await expect(page.getByRole('banner').getByRole('heading', { name: 'Web Application' })).toBeVisible();
-});
-*/
